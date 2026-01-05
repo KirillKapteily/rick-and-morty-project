d614b017f0cb692238697d2de877033ae96dfc04
Кирилл Каптелий <161931268+KirillKapteily@users.noreply.github.com>
2026-01-05 15:29:32 +0200

    Fixed title in the episodes sect, added colors to back-char
    Fixed title in the episodes sect, added colors to back-char

--- changeimages.js
+++ changeimages.js
@@ -4,6 +4,7 @@ let titleA2 = document.querySelector(".title-a-2");
 let titleA3 = document.querySelector(".title-a-3");
 let titleA4 = document.querySelector(".title-a-4");
 let titleA5 = document.querySelector(".title-a-5");
+let backgroundChar = document.querySelector(".background-char")
 let mainCharactersItem = document.querySelector(".main-characters-title")
 let mainCharactersTitle = document.querySelectorAll(".main-characters-title")
 let mainPictures = document.querySelector(".main-pictures");
@@ -11,6 +12,7 @@ let mainPictures = document.querySelector(".main-pictures");
 
 let selectImg1 = () => {
     titleA1.style.color = "rgba(161, 215, 55, 1)";
+    backgroundChar.style.backgroundColor = "#A1D737";
     mainPictures.innerHTML = ` <picture class="main-pictures">
                                 <source srcset="./kirillimg/rick@2x.png"
                                     media="(min-width: 320px) and (max-width:620px)">
@@ -23,6 +25,7 @@ let selectImg1 = () => {
 
 let selectImg2 = () => {
       titleA2.style.color = "rgba(161, 215, 55, 1)";
+       backgroundChar.style.backgroundColor = "rgba(13, 23, 29, 1)";   //
     mainPictures.innerHTML = ` <picture class="main-pictures">
                                 <source srcset="./kirillimg/charmainpic/mortysmith@1x.png"
                                     media="(min-width: 320px) and (max-width:620px)">
@@ -35,6 +38,7 @@ let selectImg2 = () => {
 
 let selectImg3 = () => {
       titleA3.style.color = "rgba(161, 215, 55, 1)";
+        backgroundChar.style.backgroundColor = "rgba(218, 248, 54, 1)";   //
     mainPictures.innerHTML = ` <picture class="main-pictures">
                                 <source srcset="./kirillimg/charmainpic/summersmith@1x.png"
                                     media="(min-width: 320px) and (max-width:620px)">
@@ -47,6 +51,7 @@ let selectImg3 = () => {
 
 let selectImg4 = () => {
       titleA4.style.color = "rgba(161, 215, 55, 1)";
+        backgroundChar.style.backgroundColor = "rgba(161, 215, 55, 1)";   //
     mainPictures.innerHTML = ` <picture class="main-pictures">
                                 <source srcset="./kirillimg/charmainpic/bethsmit@1x.png"
                                     media="(min-width: 320px) and (max-width:620px)">
@@ -59,6 +64,7 @@ let selectImg4 = () => {
 
 let selectImg5 = () => {
       titleA5.style.color = "rgba(161, 215, 55, 1)";
+        backgroundChar.style.backgroundColor = "rgba(13, 23, 29, 1)";   //
     mainPictures.innerHTML = ` <picture class="main-pictures">
                                 <source srcset="./kirillimg/charmainpic/jerrysmith@1x.png"
                                     media="(min-width: 320px) and (max-width:620px)">
